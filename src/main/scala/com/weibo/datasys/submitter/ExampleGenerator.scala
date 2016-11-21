7daa0c4baa9e74888a18634a47fe9a93a6021491
tuoyu <tuoyu@staff.weibo.com>
2016-11-21 12:31:36 +0800

    init
    init commit

---
+++ src/main/scala/com/weibo/datasys/submitter/ExampleGenerator.scala
@@ -0,0 +1,10 @@
+package com.weibo.datasys.submitter
+
+/**
+  * Created by tuoyu on 11/21/16.
+  * 生成配置文件的样例给用户，方便用户填写
+  */
+object ExampleGenerator {
+
+
+}
---
+++ src/main/scala/com/weibo/datasys/submitter/Main.scala
@@ -0,0 +1,10 @@
+package com.weibo.datasys.submitter
+
+/**
+  * Created by tuoyu on 11/21/16.
+  */
+object Main {
+  def main(args: Array[String]): Unit = {
+
+  }
+}
